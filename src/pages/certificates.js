2d9d2e4e8cc21bbe5ff5184bc7de5004760c00eb
Igor Tatarinov <igor.tanv@gmail.com>
2021-02-04 12:14:00 +0700

    progress: jpg images rendered

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -4,87 +4,43 @@ import { PageLayout, PageTitle, CertificateLink } from "../components"
 import { SEO, Utils } from "../utils"
 import Container from "react-bootstrap/Container"
 
+//modeled after blog.js
 export default ({ data }) => {
-  const allCertificates = data.allMarkdownRemark.edges || []
   const allFeaturedImages = data.allFile.edges || []
-  const regex = /\/[certificates].*\/|$/
-  const featuredImageMap = Utils.getImageMap(allFeaturedImages, regex, true, 3)
+  console.log(allFeaturedImages, 10)
 
   return (
     <PageLayout>
       <SEO title="Certificates" />
       <PageTitle title="Certificates" />
-      {/* <Container className="text-left">
-        <section>
-          {allCertificates.map(({ node }) => (
-            <div key={node.id} className="p-3">
-              <CertificateLink
-                to={node.fields.slug}
-                featuredImages={featuredImageMap[node.fields.slug]}
-                title={node.frontmatter.title}
-                tags={node.frontmatter.tags}
-                excerpt={node.excerpt}
-              />
-              <hr />
-            </div>
-          ))}
-        </section>
-      </Container> */}
-      <Container
-        fluid
-        className="p-3 w-auto text-left d-flex flex-wrap justify-content-center"
-      >
-        {allCertificates.map(({ node }) => (
+      <Container className="text-left">
+        {allFeaturedImages.map(({ node }) => (
           <div key={node.id} className="p-3">
             <CertificateLink
-              to={node.fields.slug}
-              featuredImage={featuredImageMap[node.fields.slug]}
-              title={node.frontmatter.title}
-              subtitle={node.frontmatter.date}
-              excerpt={node.excerpt}
+              featuredImage={node.childImageSharp.fluid}
             />
           </div>
         ))}
       </Container>
+
     </PageLayout>
   )
 }
 
 export const query = graphql`
   query {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/certificates/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      totalCount
+    allDirectory(filter: {relativePath: {eq: "content/certificates"}}) {
       edges {
         node {
           id
-          timeToRead
-          frontmatter {
-            title
-            description
-            tags
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
         }
       }
     }
-    allFile(
-      filter: {
-        extension: { eq: "png" }
-        relativePath: { regex: "/feature/" }
-        relativeDirectory: { regex: "/content/certificates/" }
-      }
-    ) {
+    allFile(filter: {extension: {eq: "jpg"}}) {
       edges {
         node {
           childImageSharp {
-            fluid(maxWidth: 200) {
+            fluid(maxWidth: 400) {
               ...GatsbyImageSharpFluid
             }
           }
