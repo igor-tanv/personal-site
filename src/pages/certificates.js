2eec580f91e47119c53824f4e95187d820792dfc
Minh Phan <minhphan2603@gmail.com>
2021-02-14 16:58:02 +0700

    feat: open pop-up image when click on certificate

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -1,13 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import { PageLayout, PageTitle, CertificateLink } from "../components"
 import { SEO, Utils } from "../utils"
-import { Container, Col, Row } from "react-bootstrap"
+import { Container, Col, Row, Modal, Image } from "react-bootstrap"
 
 //modeled after blog.js
 export default ({ data }) => {
   const allFeaturedImages = data.allFile.edges || []
-
+  const [showedImage, setShowedImage] = useState(false);
   return (
     <PageLayout>
       <SEO title="Certificates" />
@@ -20,9 +20,21 @@ export default ({ data }) => {
           <div key={node.id} className="p-3">
             <CertificateLink
               featuredImage={node.childImageSharp.fluid}
+              handleClickImage={() => {
+                setShowedImage(node.childImageSharp.fluid)
+              }}
             />
           </div>
         ))}
+        <Modal show={!!showedImage} onHide={() => setShowedImage(false)} centered dialogClassName="modal-certificate">
+          <Modal.Header closeButton>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Image srcSet={showedImage.srcSet} width="100%"></Image>
+            </Container>
+          </Modal.Body>
+        </Modal>
       </Container>
     </PageLayout>
   )
