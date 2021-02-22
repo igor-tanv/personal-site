03269f0225b50b3467480d7c5ab9e688b3952d0d
Igor Tatarinov <igor.tanv@gmail.com>
2021-02-22 09:15:13 +0700

    favicon update

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `standalone`,
-        icon: `${__dirname}/static/icons/darth-vader.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/static/icons/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
